4ec0f1add759307b9e8af143e24100e9674560da
Arpit Jana <arpitjana2103@gmail.com>
2026-03-02 15:36:48 +0530

    specials .ts updated

--- src/03-special.ts
+++ src/03-special.ts
@@ -1,8 +1,12 @@
-let str: string = "Arpit";
-str = "";
-
+// never : function will never return anything
 function fnc(msg: string): never {
     throw new Error(msg);
 }
 
-fnc("An errro occured");
+// void : function doesn't return a useful value
+function log(msg: string) {
+    console.log(msg);
+}
+
+// ** Never Every use 'Any' as return type
+const valueAny: any = JSON.parse;
